7edcf148ac6c50109c3136b4e4d8ee703b23ac71
Andrew Biron <andrewbiron@andrews-air.home>
2024-02-26 09:45:13 -0500

    added login-routes file

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const loginRoutes = require('./login-routes');
 
 router.use('/', homeRoutes);
+router.use('/login', loginRoutes);
+
 router.use('/api', apiRoutes);
 
 module.exports = router;
---
+++ controllers/login-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+//const { User, Post, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    // If a session exists, redirect the request to the homepage
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+  module.exports = router;
