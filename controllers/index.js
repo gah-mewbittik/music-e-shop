ef9a176311d823b885e6841c141c3926c51b32a5
Andrew Biron <andrewbiron@andrews-air.home>
2024-02-26 09:56:47 -0500

    added order route and page

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,12 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const loginRoutes = require('./login-routes');
 const accountRoutes = require('./account-routes');
+const orderRoutes = require('./order-routes');
 
 router.use('/', homeRoutes);
 router.use('/login', loginRoutes);
 router.use('/account', accountRoutes);
+router.use('/order', orderRoutes);
 
 router.use('/api', apiRoutes);
 
