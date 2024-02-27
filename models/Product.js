8abfc960b49289ce74cac5a2ea52abdf662069ce
Andrew Biron <andrewbiron@andrews-air.home>
2024-02-26 19:26:25 -0500

    Added to product model and moved css file and logo to public folder

--- models/Product.js
+++ models/Product.js
@@ -5,7 +5,28 @@ const sequelize = require('../config/connection');
 class Product extends Model {};
 
 Product.init({
+id: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  primaryKey: true,
+  autoIncrement: true,
+},
+name: {
+  type: DataTypes.STRING,
+  allowNull: false,
+},
+description: {
+  type: DataTypes.STRING,
+  allowNull: false,
+},
+price: {
+  type: DataTypes.DECIMAL(10, 2),
 
+}, 
+featured: {
+  type: DataTypes.BOOLEAN,
+},
+//TODO: Image to add.
 },
 {
     sequelize,
